3f8144710b4cb3fb2be6b4283e065e7daadcf9e4
jinwoo123456 <rlawlsdn1020@gmail.com>
2025-02-04 03:04:00 +0900

    컴포넌트 꽂는곳 입력,컴포넌트 이름 수정

--- front-end/src/pages/main/mainPage.jsx
+++ front-end/src/pages/main/mainPage.jsx
@@ -4,36 +4,36 @@ import React from 'react';
 function mainPage(){
   return (
     <div style={{justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
-      <div className="header">123123</div>
-    <div className="main-container">
-      <div className="left-panel">
+      <div className="layout__header">컴포넌트</div>
+    <div className="layout__main-container">
+      <div className="layout__left-panel">
         {/* 메인 배너너 */}
-    <div className="main-banner">123</div>
+    <div className="layout__main-banner">컴포넌트 꽂는곳</div>
     {/* 나의 도움이 필요한 모금함(카테고리) */ }
-    <div className="my-help-category">123</div>
+    <div className="layout__my-help-category">컴포넌트 꽂는곳</div>
     {/* 가장 많이 기부중인 모금함 */}
-    <div className="top-campaign">123</div>
+    <div className="layout__top-campaign">컴포넌트 꽂는곳</div>
 
     {/* 후원 리스트 */}
-    <div className="main-campaign-list">123</div>
+    <div className="layout__main-campaign-list">컴포넌트 꽂는곳</div>
     
 
       </div>
-      <div className="right-panel">
-        <div className='first-section'>
-    <div className="total-donation"> 
-      12</div>
-    <div className="donation-review"> 
-     12 </div>
+      <div className="layout__right-panel">
+        <div className='layout__first-section'>
+    <div className="layout__total-donation"> 
+      컴포넌트 꽂는곳</div>
+    <div className="layout__donation-review"> 
+    컴포넌트 꽂는곳</div>
     </div>
-    <div className="new-campaign">
-      123
+    <div className="layout__new-campaign">
+    컴포넌트 꽂는곳
     </div>
-    <div className="social-link">
-      123123123
+    <div className="layout__social-link">
+    컴포넌트 꽂는곳
     </div>
 
-    <div className="receive-donation">123</div>
+    <div className="layout__receive-donation">컴포넌트 꽂는곳</div>
       </div>
   </div></div>
   );
