67faf8a35d09b06a3be8036fe1b6f43de989b3e9
jinwoo123456 <rlawlsdn1020@gmail.com>
2025-02-04 18:31:11 +0900

    card1,2,3 수정 완료

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RegistType from "./pages/auth/registType";
+import Card3 from "./component/card3";
 import "./style/scss/style.scss"; // 글로벌 스타일 적용
-import Favicon from "./component/logo/favicon"; 
-import MainPage from './pages/main/mainPage';
-import CategoryPage from './pages/category/categoryPage';
-import Login from "./pages/auth/login";
-import Signup from "./pages/auth/signup";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <Signup /> App.jsx 대신 Signup.jsx만 렌더링 */}
-    <Signup />
+    <Card3 />
   </React.StrictMode>
 );
