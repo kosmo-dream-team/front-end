7d3c502f2362f1068e49a9a216780339b3cdb570
jinwoo123456 <rlawlsdn1020@gmail.com>
2025-02-06 14:50:20 +0900

    푸쉬문제발생

--- front-end/src/pages/main/mainPage.jsx
+++ front-end/src/pages/main/mainPage.jsx
@@ -1,46 +1,44 @@
-import React from 'react';
-import  Header  from "../../component/header";
- import "../../style/scss/style.scss";
-function mainPage(){
+import React from "react";
+import Header from "../../component/header";
+import "../../style/scss/style.scss";
+function mainPage() {
   return (
-    <div style={{justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
+    <div
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <div className="layout__header">
-        <Header /></div>
-    <div className="layout__main-container">
-      <div className="layout__left-panel">
-        {/* 메인 배너너 */}
-    <div className="layout__main-banner">컴포넌트 꽂는곳</div>
-    {/* 나의 도움이 필요한 모금함(카테고리) */ }
-    <div className="layout__my-help-category">컴포넌트 꽂는곳</div>
-    {/* 가장 많이 기부중인 모금함 */}
-    <div className="layout__top-campaign">컴포넌트 꽂는곳123123</div>
-
-    {/* 후원 리스트 */}
-    <div className="layout__main-campaign-list">컴포넌트 꽂는곳</div>
-    
-
+        <Header />
       </div>
-      <div className="layout__right-panel">
-        <div className='layout__first-section'>
-    <div className="layout__total-donation"> 
-      컴포넌트 꽂는곳</div>
-    <div className="layout__donation-review"> 
-    컴포넌트 꽂는곳</div>
-    </div>
-    <div className="layout__new-campaign">
-    컴포넌트 꽂는곳
-    </div>
-    <div className="layout__social-link">
-    컴포넌트 꽂는곳
-    </div>
+      <div className="layout__main-container">
+        <div className="layout__left-panel">
+          {/* 메인 배너너 */}
+          <div className="layout__main-banner">컴포넌트 꽂는곳</div>
+          {/* 나의 도움이 필요한 모금함(카테고리) */}
+          <div className="layout__my-help-category">컴포넌트 꽂는곳</div>
+          {/* 가장 많이 기부중인 모금함 */}
+          <div className="layout__top-campaign">컴포넌트 꽂는곳123123</div>
 
-    <div className="layout__receive-donation">컴포넌트 꽂는곳</div>
+          {/* 후원 리스트 */}
+          <div className="layout__main-campaign-list">컴포넌트 꽂는곳</div>
+        </div>
+        <div className="layout__right-panel">
+          <div className="layout__first-section">
+            <div className="layout__total-donation">컴포넌트 꽂는곳</div>
+            <div className="layout__donation-review">컴포넌트 꽂는곳</div>
+          </div>
+          <div className="layout__new-campaign">컴포넌트 꽂는곳</div>
+          <div className="layout__social-link">컴포넌트 꽂는곳</div>
+
+          <div className="layout__receive-donation">컴포넌트 꽂는곳</div>
+        </div>
       </div>
-  </div></div>
+    </div>
   );
-};
+}
 
 export default mainPage;
-
-
-
