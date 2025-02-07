3c8e6e20f29be619c9adda977f7cfcacd51727e7
jinwoo123456 <rlawlsdn1020@gmail.com>
2025-02-08 02:29:21 +0900

    메인페이지 리스트 모바일 버전 수정중

--- front-end/src/component/main/MainCampaignList.jsx
+++ front-end/src/component/main/MainCampaignList.jsx
@@ -11,17 +11,17 @@ export default function MainCampaignList() {
       />
      
       <div className="layout__main-campaign-list__content">
-        <div >
+        <div className="main-campaign-list__title" >
           <div className="main-campaign-list__category">
             <div className="main-campaign-list__category-text">
               아동 / 청소년
             </div>
           </div>
           <div className="main-campaign-list__quote">
-            “ 예비 초등학생에게 책가방을 보내주세요 ”
+            “ 예비 초등학생에게 책가방을 보내주세요sd ”
           </div>
           <div className="main-campaign-list__beneficiary">
-            <div className="main-campaign-list__beneficiary-text">수혜asddddddddddddddddddsddddddddddddddddddddddsddddddddddddddddddddddddddddddddddddddddddddddd자명</div>
+            <div className="main-campaign-list__beneficiary-text">수혜as자명</div>
           </div>
         </div>
         <div className="main-campaign-list__deadline">
@@ -29,12 +29,12 @@ export default function MainCampaignList() {
         </div>
       </div>
       <div className="main-campaign-list__progress-wrapper">
+        <div className="main-campaign-list__progress-bar">
+          <div className="main-campaign-list__progress-fill" />
+        </div>
         <div className="main-campaign-list__progress-percent">33%</div>
         <div className="main-campaign-list__amount">
           <span>1,920,999 원</span>
-        </div>
-        <div className="main-campaign-list__progress-bar">
-          <div className="main-campaign-list__progress-fill" />
         </div>
       </div>
    
