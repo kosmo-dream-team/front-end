4e6eb71ef464230568a06c11436250a82f6bbdf6
jinwoo123456 <rlawlsdn1020@gmail.com>
2025-02-07 18:30:07 +0900

    메인 페이지 추가 - 섹션1 - 배너1,캠페인인기리스트 캠페인리스트

--- front-end/src/pages/main/mainPage.jsx
+++ front-end/src/pages/main/mainPage.jsx
@@ -1,6 +1,9 @@
+import MainBanner from "../../assets/img/main-banner1.png";
 import Navbar from "../../component/common/navbar/Navbar";
+import MainCampaignList from "../../component/main/MainCampaignList";
+import MainCategory from "../../component/main/MainCategory";
+import PopularCampaign from "../../component/main/PopularCampaign";
 import "../../style/scss/style.scss";
-
 function MainPage() {
   return (
     <div
@@ -16,14 +19,24 @@ function MainPage() {
       <div className="layout__main-container">
         <div className="layout__left-panel">
           {/* 메인 배너너 */}
-          <div className="layout__main-banner">컴포넌트 꽂는곳</div>
+          <div className="layout__main-banner">
+            <a href="/">
+              <img src={MainBanner} alt="" style={{ width: "100%" }} />
+            </a>
+          </div>
           {/* 나의 도움이 필요한 모금함(카테고리) */}
-          <div className="layout__my-help-category"></div>
+          <div className="layout__my-help-category">
+            <MainCategory style={{ width: "100%" }} />
+          </div>
           {/* 가장 많이 기부중인 모금함 */}
-          <div className="layout__top-campaign">컴포넌트 꽂는곳123123</div>
+          <div className="layout__top-campaign">
+            <PopularCampaign />
+          </div>
 
           {/* 후원 리스트 */}
-          <div className="layout__main-campaign-list">컴포넌트 꽂는곳</div>
+          <div className="layout__main-campaign-list">
+            <MainCampaignList />
+          </div>
         </div>
         <div className="layout__right-panel">
           <div className="layout__first-section">
--- front-end/src/store/useNavbarStore.jsx
+++ front-end/src/store/useNavbarStore.jsx
@@ -3,13 +3,14 @@ import { create } from "zustand";
 import CartIco from "../assets/img/cart.png";
 import SearchIco from "../assets/img/search.svg";
 import Favicon from "../component/logo/Favicon";
-
+//navbar에 사용되는 데이터들. desktop,mobile 공통
 const useMenuStore = create(() => ({
   menuDb: {
-    pageName: "GoodMemory",
+    pageName: "GoodMemory", //페이지이름 x
     mainPage: "/pages/main/mainPage", // react-router-dom에서는 절대 경로 사용을 권장합니다.
-    logoTheme: <Favicon />,
+    logoTheme: <Favicon />, //favicon
     menuList: [
+      //메뉴
       { name: "홈", url: "/pages/main/mainPage" },
       { name: "회사 소개", url: "/pages/company" },
       { name: "카테고리", url: "/pages/category/CategoryPage" },
