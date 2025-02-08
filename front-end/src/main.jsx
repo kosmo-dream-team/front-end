ede6bc3d31a3b36a079e0958a5255678a7bbf987
jinwoo123456 <rlawlsdn1020@gmail.com>
2025-02-08 17:32:49 +0900

    경민형과 병합 전 커밋

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Card1 from "./component/main/card1";
+import Card2 from "./component/main/card2";
 import Card3 from "./component/main/card3";
+import Card4 from "./component/main/card4";
 import RegistType from "./pages/auth/registType";
 import Signup from "./pages/auth/signup";
-import MainCategory from "./pages/main/mainPage";
+import MainPage from "./pages/main/mainPage";
 import "./style/scss/style.scss"; // 글로벌 스타일 적용
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -12,7 +15,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* <Signup /> App.jsx 대신 Signup.jsx만 렌더링 */}
     <Router>
       <Routes>
-        <Route path="/ca" element={<MainCategory />} />
+        <Route path="/cd1" element={<Card1 />} />
+        <Route path="/cd2" element={<Card2 />} />
+        <Route path="/cd3" element={<Card3 />} />
+        <Route path="/cd4" element={<Card4 />} />
+        <Route path="/main" element={<MainPage />} />
         <Route path="/registType" element={<RegistType />} />
         <Route path="/registType/signup" element={<Signup />} />
       </Routes>
