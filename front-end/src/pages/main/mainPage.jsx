795c2360a099c4f9bf7a87804282468bec06d1db
jinwoo123456 <rlawlsdn1020@gmail.com>
2025-02-10 12:58:22 +0900

    메인페이 디자인 완성.반응형

--- front-end/src/pages/main/mainPage.jsx
+++ front-end/src/pages/main/mainPage.jsx
@@ -1,14 +1,15 @@
 import MainBanner from "../../assets/img/main-banner1.png";
+import Footer from "../../component/common/footer/Footer";
 import Navbar from "../../component/common/navbar/Navbar";
+import Card2 from "../../component/main/Card2";
+import Card3 from "../../component/main/Card3";
+import Card4 from "../../component/main/card4";
+import Card5 from "../../component/main/Card5";
 import MainCampaignList from "../../component/main/MainCampaignList";
 import MainCategory from "../../component/main/MainCategory";
 import PopularCampaign from "../../component/main/PopularCampaign";
 import TotalDonation from "../../component/main/TotalDonation";
-import Card2 from "../../component/main/Card2";
 import "../../style/scss/style.scss";
-import Card3 from "../../component/main/Card3";
-import Card4 from "../../component/main/card4";
-import Card5 from "../../component/main/Card5";
 function MainPage() {
   return (
     <div
@@ -20,7 +21,6 @@ function MainPage() {
       }}
     >
       <Navbar />
-
       <div className="layout__main-container">
         <div className="layout__left-panel">
           {/* 메인 배너너 */}
@@ -31,7 +31,7 @@ function MainPage() {
           </div>
           {/* 나의 도움이 필요한 모금함(카테고리) */}
           <div className="layout__main-category">
-            <MainCategory  />
+            <MainCategory />
           </div>
           {/* 가장 많이 기부중인 모금함 */}
           <div className="layout__top-campaign">
@@ -45,16 +45,27 @@ function MainPage() {
         </div>
         <div className="layout__right-panel">
           <div className="layout__first-section">
-            <div className="layout__total-donation"><TotalDonation/></div>
-            <div className="layout__donation-review"><Card2/></div>
+            <div className="layout__total-donation">
+              <TotalDonation />
+            </div>
+            <div className="layout__donation-review">
+              <Card2 />
+            </div>
           </div>
-         
-          <div className="layout__new-campaign"><Card3/></div>
-          <div className="layout__social-link"><Card4/></div>
 
-          <div className="layout__receive-donation"><Card5/></div>
+          <div className="layout__new-campaign">
+            <Card3 />
+          </div>
+          <div className="layout__social-link">
+            <Card4 />
+          </div>
+
+          <div className="layout__receive-donation">
+            <Card5 />
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
