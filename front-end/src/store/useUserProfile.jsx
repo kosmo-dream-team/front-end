931d65813c572580c6c528cb28ddbadcad4591d5
jinwoo123456 <rlawlsdn1020@gmail.com>
2025-02-11 18:28:23 +0900

    api/zutand store 수정완료 내일 test 예정

--- front-end/src/store/useUserProfile.jsx
+++ front-end/src/store/useUserProfile.jsx
@@ -2,14 +2,26 @@ import axios from "axios";
 import create from "zustand";
 
 const useUserProfile = create((set) => ({
-  userProfile: null,
-
-  fetchUserProfile: async () => {
-    const response = await axios.get("/api/user-profile");
-    set({ userProfile: response.data });
-
-    console.log(response.date);
+  userProfile: {
+    nickname: "",
+    password: "",
+    email: "",
+    age: "",
+    sex: "",
+    rank: "",
+    totalDonationCount: "",
+    profileImage: "",
   },
+  fetchUserProfile: async () => {
+    try {
+      const response = await axios.get("/api/userprofile");
+      set({ userProfile: response.data });
+    } catch (error) {
+      console.error("Failed to fetch user profile:", error);
+    }
+  },
+  setUserProfile: (newProfile) =>
+    set((state) => ({ userProfile: { ...state.userProfile, ...newProfile } })),
 }));
 
 export default useUserProfile;
