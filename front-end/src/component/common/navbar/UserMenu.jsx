0b81edf9fbb640ba2c0b7e1fb7d0e9a37dcf488a
jinwoo123456 <rlawlsdn1020@gmail.com>
2025-02-12 17:06:47 +0900

    fix-router 오류 수정

--- front-end/src/component/common/navbar/UserMenu.jsx
+++ front-end/src/component/common/navbar/UserMenu.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function UserMenu() {
   return (
     <>
-      <Link to="/profile" className="nav-list">
+      <Link to="/myPage" className="nav-list">
         마이페이지
       </Link>
       <Link to="/logout" className="nav-list">
